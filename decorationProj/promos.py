5186456aaccee1ee0906c6bc120c100d97ed84bd
KongWiki <kongwiki@163.com>
2019-09-03 14:06:01 +0800

    装饰器..

--- decorationProj/promos.py
+++ decorationProj/promos.py
@@ -4,3 +4,32 @@
 @File: promos.py
 @Email: kongwiki@163.com
 """
+promos = []
+
+def promotion(promo_func):
+	promos.append(promo_func)
+	return promo_func
+
+
+@promotion
+def fidelity(order):
+	"""
+	为积分为1000或以上的顾客提供5%的折扣
+	:param order:
+	:return:
+	"""
+	return  order.total()*0.5 if order.customer.fidelity >= 1000 else 0
+
+@promotion
+def bulk_item(order):
+	"""
+	单个商品为20个或以上的提供20%的折扣
+	:param order:
+	:return:
+	"""
+	discount = 0
+	for item in order.cart:
+		if item.quantity >= 20:
+			discount += item.total()*.1
+	return discount
+
--- decorationProj/registration.py
+++ decorationProj/registration.py
@@ -4,3 +4,36 @@
 @File: registration.py
 @Email: kongwiki@163.com
 """
+registry = []
+
+
+def register(func):
+	print("runnnig register(%s)" % func)
+	registry.append(func)
+	return func
+
+
+@register
+def f1():
+	print("running f1()")
+
+
+@register
+def f2():
+	print("running f2()")
+
+
+def f3():
+	print("running f3()")
+
+
+def main():
+	print("running main()")
+	print("registry ->", registry)
+	f1()
+	f2()
+	f3()
+
+
+if __name__ == '__main__':
+	main()
