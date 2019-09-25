c18355c09e239297bfd017434d9b909fe2dd53e6
KongWiki <kongwiki@163.com>
2019-09-25 20:39:15 +0800

    添加迭代器&迭代对象&生成器README.md

--- iteratorPattern/sentence.py
+++ iteratorPattern/sentence.py
@@ -6,6 +6,7 @@
 """
 import re
 import reprlib
+from collections import abc
 
 RE_WORD = re.compile('\w+')
 
@@ -25,11 +26,39 @@ class Sentence:
 		return 'Sentence(%s)' % reprlib.repr(self.text)
 
 
+# 创建可迭代对象
+class Foo:
+	def __iter__(self):
+		pass
+
+
 if __name__ == '__main__':
-	s = Sentence('"The time has come," the Walrus said.')
-	print(s)
+	# s = Sentence('"The time has come," the Walrus said.')
+	# print(s)
 	# 输出为Sentence('"The time ha... Walrus said.')的原因为 sentence类
 	# 中的__repr__方法
-	for word in s:
-		print(word)
-	print(list(s))
+	# for word in s:
+	# 	print(word)
+	# print(list(s))
+	# print(iter(s))
+	# print(isinstance(Foo, abc.Iterable))
+	# f = Foo()
+	# print(isinstance(f, abc.Iterable))
+	s = 'ABC'
+	# for char in s:
+	# 	print(char)
+	# it = iter(s)
+	# while True:
+	# 	try:
+	# 		print(next(it))
+	# 	except:
+	# 		del it
+	# 		break
+	s3 = Sentence('pig and pepper')
+	it = iter(s3)
+	print(it)
+	print(next(it))
+	print(next(it))
+	print(next(it))
+	print(list(it))
+	print(list(iter(s3)))
